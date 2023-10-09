5700f1c17a6a800a0e83892e9daa144275cb6d13
mananalepele <seipatiannah1@gmail.com>
2023-10-09 07:09:55 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/*
+ * main - prints exactly programming is like building a multilingual puzzle
+ *
+ * return - Always zero
+ */
+
+int main(void)
+
+{
+puts("\"Programming is like building a multilingual puzzle");
+
+return (0);
+}
+
