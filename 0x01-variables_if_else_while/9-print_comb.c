4e93a6dfca831aead89e61837bc4a8a61d5e3aef
mananalepele <seipatiannah1@gmail.com>
2023-11-01 09:52:14 -0700

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - prints all combinations os single digit  numbers
+ * Numbers are separated by , and  space
+ * Return: Always 0
+ */
+
+int main(void)
+{
+int n;
+for (n = 0; n < 10; n++)
+{
+putchar(',');
+putchar(' ');
+putchar('0' + n);
+}
+putchar('\n');
+return (0);
+}
+
