277f569cfa547918fef2bbc85f728291ca62811d
mananalepele <seipatiannah1@gmail.com>
2023-11-30 04:00:33 -0800

    102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -11,32 +11,20 @@ int main(void)
 {
 int num1;
 int num2;
-int num3;
-int num4;
 
-for (num1 = 0; num1 < 10; num1++)
+for (num1 = 0; num1 <= 99; num1++)
 {
-for (num2 = num1 + 1; num2 < 10; num2++)
+for (num2 = num1 + 0; num2 <= 99; num2++)
 {
-putchar('0' + num1);
-putchar('0' + num2);
-
-if (num1 != 9 || num2 != 8)
-{
+putchar((num1 / 10) + '0');
+putchar((num1 % 10) + '0');
 putchar(' ');
-}
-for (num3 = 0; num3 < 10; num3++)
-{
-for (num4 = num3 + 1; num4 < 10; num4++)
-{
-putchar('0' + num3);
-putchar('0' + num4);
-if (num3 != 9 || num4 != 9)
+putchar((num2 / 10) + '0');
+putchar((num2 % 10) + '0');
+if (num1 != 98 || num2 != 99)
 {
 putchar(',');
 putchar(' ');
-}
-}
 }
 }
 }
