4832e94e5fd8c5f0e06a2fcaa6a63bda6b0cf78f
mananalepele <seipatiannah1@gmail.com>
2023-12-01 00:59:24 -0800

    8-24_hours.c

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * jack_bauer - prints every minute of the day
+ *
+ * Return: Always 0
+ */
+
+void jack_bauer(void)
+{
+int hrs;
+int min;
+
+for (hrs = 0; hrs <= 23; hrs++)
+{
+for (min = 0; min <= 59; min++)
+{
+_putchar((hrs / 10) + '0');
+_putchar((hrs % 10) + '0');
+_putchar(':');
+_putchar((min / 10) + '0');
+_putchar((min % 10) + '0');
+if (min != 59)
+{
+_putchar('\n');
+}
+}
+}
+_putchar('\n');
+return;
+}
+
